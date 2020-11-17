5d2ad7f8873134a0b83461995338d2bde593e537
Cooper Tingey <cooper.tingey@gmail.com>
2020-11-17 13:23:11 -0700

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Cooper Tingey Portfolio</h1>
+        <div>{moment().format('MMM Do YYY, h:mm:ss a')}</div>
+      </div>
+    );
+  }
+}
